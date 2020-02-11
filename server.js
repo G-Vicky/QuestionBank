537c37456128a2a431451ba29789a5ce30178dbd
G-Vicky <vigneshwaran2747@gmail.com>
2020-02-11 10:01:34 +0530

    added mysql

--- server.js
+++ server.js
@@ -1,24 +1,66 @@
-const express = require('express');
+var mysql = require("mysql");
+const express = require("express");
 const app = express();
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "admission"
+});
+
+connection.connect(err => {
+  if (err) {
+    console.log(err);
+  }
+  //   console.log("connected as id " + connection.threadId);
+});
 
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello Wolrd');
+const papers = {
+  data: [
+    {
+      id: "1",
+      name: "Question Paper1"
+    },
+    {
+      id: "2",
+      name: "Question Paper2"
+    },
+    {
+      id: "3",
+      name: "Question Paper3"
+    }
+  ]
+};
+
+app.get("/", (req, res) => {
+  res.send("Hello Wolrd");
 });
 
-app.get('/home', (req, res) => {
-    res.send('Server from home');
+app.get("/home", (req, res) => {
+  res.send("Server from home");
 });
 
-app.get('/register', (req, res) => {
-    res.send('server from register');
+app.get("/register", (req, res) => {
+  connection.query("SELECT * from registration", function(err, rows, fields) {
+    if (err) {
+      throw err;
+    }
+
+    console.log("The solution is: ", rows);
+    res.send("server from register" + JSON.stringify(rows));
+  });
+
+  connection.end();
 });
 
-app.get('/questionpaper/:class', (req, res) => {
-    res.send('Server question Paper: ' + req.params.class);
+app.get("/questionpaper/:class", (req, res) => {
+  res.setHeader("Content-Type", "application/json");
+  res.send(papers);
 });
 
 app.listen(PORT, () => {
-    console.log(`Listening on ${PORT}...`);
+  console.log(`Listening on ${PORT}...`);
 });
